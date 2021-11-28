255af0897f18bc49bc1afa0d1480c8497475fd71
Oluwatuyi Olowoyeye <77670180+OSO3670@users.noreply.github.com>
2021-11-27 18:51:30 -0600

    Update site.js

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ $.extend($.easing,
     easeInOutExpo: function (x, t, b, c, d) {
         if (t==0) return b;
         if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+        if ((t/=d/2) < 2) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
         return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
     }
 });
